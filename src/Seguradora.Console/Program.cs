7a9278d9d14573b83a146490eff4023ca2b266b9
David Oliveira <iniciativad@gmail.com>
2026-09-16 11:01:18 -0300

    apolice removido

--- src/Seguradora.Console/Program.cs
+++ src/Seguradora.Console/Program.cs
@@ -9,20 +9,3 @@ var maria = new Segurado
 
 Console.WriteLine($"{maria.Nome}, {maria.Idade} anos");
 Console.WriteLine($"Maior de idade: {maria.MaiorDeIdade}");
-
-
-//2
-var apolice = new ApoliceAutomovel
-{
-Numero = "AUTO-001", Segurado = maria, ValorSegurado = 85_000m,
-Placa = "ABC1D23", AnoFabricacao = 2012
-};
-
-apolice.AdicionarCobertura(new Cobertura
-{ 
-    Tipo = TipoCobertura.Colisao, 
-    Descricao = "Colisão", 
-    LimiteIndenizacao = 85_000m 
-});
-apolice.Ativar();
-Console.WriteLine($"{apolice.Situacao}, prêmio {apolice.CalcularPremio():C}");
